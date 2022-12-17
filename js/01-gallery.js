96bbc08124aa2c5fadbf87932aa341053263367e
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-17 17:57:26 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalImage = event.target.getAttribute('data-source');
+
+  const instance = basicLightbox.create(`
+  <img src="${originalImage}" width="800" height="600">
+`)
+
+instance.show()
+
+});
+
